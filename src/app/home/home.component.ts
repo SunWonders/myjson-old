5e7d7db21d0b62d10b91835324c5848e880827b0
dilipduraiswamy <dilipduraiswamy@gmail.com>
2022-03-19 21:53:29 +0530

    ishome enabled false

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,7 +8,7 @@ import { environment } from 'src/environments/environment';
 })
 export class HomeComponent implements OnInit {
 
-  isEnabled:boolean= environment.isPutEnabled;
+  isEnabled:boolean= environment.isHomeEnabled;
 
   constructor() { 
   
